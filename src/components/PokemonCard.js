7d32501fb59577d9c186b3a65a5433fe3baf3385
EvgeniyHalimon <halimonevgeniy@gmail.com>
2021-11-17 14:00:58 +0200

    qqq

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -1,15 +1,28 @@
 import React from 'react';
 import { fetchPokemons } from '../utils/FetchPokemons';
+import {Card, ListGroup, ListGroupItem} from 'react-bootstrap'
+
 
 const url = `http://pokeapi.co/api/v2/pokemon/?limit=898`
 const url1 = `https://pokeapi.co/api/v2/pokemon/?limit=10&offset=0`
 
 
-
-
-function PokemonCard(){
+function PokemonCard({name}){
     return(
-        <h1>CARD</h1>
+        <Card style={{ width: '18rem' }}>
+            {/* <Card.Img variant="top" src={sprites.front_default} />
+            <Card.Body>
+                <Card.Title>{name}</Card.Title>
+                <Card.Title>{order}</Card.Title>
+                <Card.Text>
+                    {types.type.name}
+                </Card.Text>
+            </Card.Body>
+            <ListGroup className="list-group-flush">
+                <ListGroupItem>{statusbar.stat.name}{statusbar.base_stat}</ListGroupItem>
+                
+            </ListGroup> */}
+        </Card>
     )
 }
 
--- src/components/PokemonsList.js
+++ src/components/PokemonsList.js
@@ -1,18 +1,41 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { fetchPokemons, paginationPokemon } from '../utils/FetchPokemons';
-/* 
+import shortid from 'shortid'
+
 const url = `http://pokeapi.co/api/v2/pokemon/?limit=898`
-const url1 = `https://pokeapi.co/api/v2/pokemon/?limit=10&offset=0` */
+const url1 = `https://pokeapi.co/api/v2/pokemon/?limit=10&offset=0`
 
-async function getPokemons(){
-    const pokemon = await paginationPokemon(10, 0)
-    const one = await fetchPokemons(`https://pokeapi.co/api/v2/pokemon/1`)
-}
 
-getPokemons()
 
 function PokemonsList(){
-    return <ul>LIST</ul>
+    const [pokemons, setPokemons] = useState([])
+
+    async function getPokemons(){
+        const pokemon = await paginationPokemon(10, 0)
+        console.log(pokemon)
+        setPokemons(pokemon.data.results)
+        console.log(pokemons)
+        const one = await fetchPokemons(`https://pokeapi.co/api/v2/pokemon/1`)
+    }
+    
+    
+    useEffect(() => {
+        getPokemons()
+    }, []);
+
+    return(
+        <ListGroup>
+            {pokemons.map(item => (
+                <ListGroupItem 
+                    key={shortid.generate()}
+                    id={item.url}
+                    onClick={(e) => console.log(e.target.id)}
+                >
+                    {item.name}
+                </ListGroupItem>
+            ))}
+        </ListGroup>
+    )
 }
 
 export default PokemonsList

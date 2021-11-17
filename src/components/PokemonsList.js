3ae3ab395293abfe654a42010a6f2bdc1193746e
EvgeniyHalimon <halimonevgeniy@gmail.com>
2021-11-17 13:23:01 +0200

    ererererer

--- src/components/PokemonsList.js
+++ src/components/PokemonsList.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { fetchPokemons, paginationPokemon } from '../utils/FetchPokemons';
-
+/* 
 const url = `http://pokeapi.co/api/v2/pokemon/?limit=898`
-const url1 = `https://pokeapi.co/api/v2/pokemon/?limit=10&offset=0`
+const url1 = `https://pokeapi.co/api/v2/pokemon/?limit=10&offset=0` */
 
 async function getPokemons(){
     const pokemon = await paginationPokemon(10, 0)

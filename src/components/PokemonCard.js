d805d637159cf962aa2970c59af6016f7b6b4728
EvgeniyHalimon <halimonevgeniy@gmail.com>
2021-11-18 00:26:08 +0200

    beautify card

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import { fetchPokemons } from '../utils/FetchPokemons';
-import {Card, Container, ListGroup, ListGroupItem} from 'react-bootstrap'
+import {Card, Container, ListGroup, Table} from 'react-bootstrap'
 import { useParams } from 'react-router';
 import shortid from 'shortid';
 
@@ -23,44 +23,60 @@ function PokemonCard(){
         setIsShow(true)
     }
 
-    console.log(pokemon.types)
-    console.log(pokemon.stats)
+    console.log(pokemon)
 
     useEffect(() => {
         handleFetch(number)
     }, []);
 
     return(
-                <Container >
-                    {isShow ? 
-                    <Card key={shortid.generate()} style={{ width: '18rem' }}>
-                        <Card.Img 
-                        style={{margin : '0 auto', width: '50%'}}
-                        variant="top" 
-                        src={pokemon.sprites.front_default} />
-                        <Card.Title>{pokemon.name}</Card.Title>
-                        <Card.Title># {pokemon.order}</Card.Title>
-                        <Card.Text>Abilities</Card.Text>
-                        <ListGroup variant="flush">
+        <Container >
+        {isShow ? 
+        <Card key={shortid.generate()} style={{ width: '18rem', margin : '0 auto' }}>
+            <Card.Img 
+            style={{margin : '0 auto', width: '60%'}}
+            variant="top" 
+            src={pokemon.sprites.front_default} />
+            <Card.Title>{pokemon.name}</Card.Title>
+            <Card.Title># {pokemon.order}</Card.Title>
+            <Container style={{display : 'flex', padding : '0'}}>
+                    <Container style={{padding: '0', width: '48%'}}>
+                        <ListGroup>
+                        <ListGroup.Item  variant='dark'>
+                            Abilities
+                        </ListGroup.Item>
                             {pokemon.abilities.map(item =>(
-                                <ListGroup.Item key={shortid.generate()}>
+                                <ListGroup.Item  variant='info' key={shortid.generate()}>
                                     {item.ability.name}
                                 </ListGroup.Item>
                             ))}
-                            {/* <ListGroupItem>{.statusbar.stat.name}{item.statusbar.base_stat}</ListGroupItem> */}
-                        </ListGroup>
-                        <ListGroup variant="flush">
-                            {/* {pokemon.abilities.map(item =>(
-                                <ListGroup.Item key={shortid.generate()}>
-                                    {item.ability.name}
+                        </ListGroup> 
+                        <ListGroup>
+                        <ListGroup.Item  variant='dark'>
+                            Type
+                        </ListGroup.Item>
+                            {pokemon.types.map(item =>(
+                                <ListGroup.Item  variant='info' key={shortid.generate()}>
+                                    {item.type.name}
                                 </ListGroup.Item>
-                            ))} */}
-                            {/* <ListGroupItem>{.statusbar.stat.name}{item.statusbar.base_stat}</ListGroupItem> */}
-                        </ListGroup>
-                    </Card> : 
-                    <h1>oops</h1>
-                    }
-                </Container>
+                            ))}
+                        </ListGroup> 
+                    </Container>
+                    <ListGroup>
+                    <ListGroup.Item variant='dark'>
+                        Stats
+                    </ListGroup.Item>
+                        {pokemon.stats.map(item =>(
+                            <ListGroup.Item  variant='info' key={shortid.generate()}>
+                                {item.stat.name} {item.base_stat}
+                            </ListGroup.Item>
+                        ))}
+                    </ListGroup> 
+            </Container>
+        </Card> : 
+        <h1>oops</h1>
+        }
+    </Container>
     )
 }
 
